e98b2e978442a5292c25c21c5025e63c17e66dc6
Guilherme Comunian <twoguini@gmail.com>
2025-09-20 13:05:24 -0300

    Button added - BlurIn Added

--- ASKTBGIRLF/src/App.tsx
+++ ASKTBGIRLF/src/App.tsx
@@ -1,18 +1,23 @@
 import { Route, Routes } from 'react-router-dom'
+import { useState } from 'react'
 
 import Login from './pages/Login'
 import './App.css'
 
 function App() {
 
+  const [ blurInBg, setBlurInBg ] = useState(false);
+  const [ blurOutBg, setBlurOutBg ] = useState(false);
+  const [ rotatingBG, setRotatingBG ] = useState(false);
+
   return (
     <>
-      <body>
-        <img src="/assets/img/BG.png" className='rotationg-bg'/>
+      <div>
+        <img src="/assets/img/BG.png" className={'rotationg-bg ' + (blurInBg? 'blurInAnimate ' : ' ') + (blurOutBg? 'blurOutAnimate ' : ' ') + (rotatingBG? 'rotationAnimate ' : ' ')}/>
         <Routes>
-          <Route path='/' element= {<Login />} />
+          <Route path='/' element= {<Login setBlurInBg={setBlurInBg} />} />
         </Routes>
-      </body>
+      </div>
     </>
   )
 }
--- ASKTBGIRLF/src/pages/Login.tsx
+++ ASKTBGIRLF/src/pages/Login.tsx
@@ -1,7 +1,21 @@
-function Login() {
+import { useState } from "react";
+
+type props = {
+  setBlurInBg: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function Login({setBlurInBg}: props) {
+
+  const [ hideElement, setHideElement ] = useState(false);
+
+  const loginFormAppearence = () => {
+    setBlurInBg(true);
+    setHideElement(true);
+  }
 
   return(
     <>
+      <button type="button" onClick={loginFormAppearence} className={"loginBtn " + (hideElement? "hideElement" : "")} >Entrar</button>
       <div>
       </div>
     </>
