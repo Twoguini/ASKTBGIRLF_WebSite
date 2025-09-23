89544323636c1ef41005dabab7aa40948bc17ccc
Guilherme Comunian <twoguini@gmail.com>
2025-09-23 13:47:54 -0300

    Login Added

--- ASKTBGIRLF/src/pages/Login.tsx
+++ ASKTBGIRLF/src/pages/Login.tsx
@@ -6,12 +6,27 @@ function Login() {
   const [ hideElement, setHideElement ] = useState(false);
   const [ displayNone, setDisplayNone ] = useState(false);
 
-  const loginFormAppearence = () => {
-    setHideElement(true);
+  const [ loginForm, setLoginForm ] = useState({
+    username: "",
+    password: ""
+  });
 
-    setTimeout(() => {
-      setDisplayNone(true);
-    }, 1000);
+  const getFormInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginForm((prev) => ({
+      ...prev,
+      [name]: value
+    }));
+  }
+
+  const loginFormAppearence = () => {
+    if (loginForm.password === "guiguiamaduda" && loginForm.username === "guilherme.comunian") {
+      setHideElement(true);
+
+      setTimeout(() => {
+        setDisplayNone(true);
+      }, 1000);
+    } else return;
   }
 
   return(
@@ -19,10 +34,10 @@ function Login() {
       <div className={"formDivBack " + (hideElement? "formFirstChangeColor" : "")}>
         <div className={"formDiv " + (hideElement? "hideElement " : "") + (displayNone? "displayNone" : "")}>
           <h1 className={"logInTitle " + (hideElement? "hideElement" : "")}>Faça Login</h1>
-          <label htmlFor="">Usuário:</label>
-          <input type="text" name="" id="" />
-          <label htmlFor="">Senha:</label>
-          <input type="password" name="" id="" />
+          <label htmlFor="username">Usuário:</label>
+          <input type="text" name="username" id="" onChange={getFormInputValue} />
+          <label htmlFor="password">Senha:</label>
+          <input type="password" name="password" id="" onChange={getFormInputValue} />
           <button type="button" onClick={loginFormAppearence} className={"loginBtn " + (hideElement? "hideElement" : "")} >Entrar</button>
         </div>
         <h1 className={"secFormTitle " + (hideElement? "" : "displayNone")}>Formulário de Segurança</h1>
